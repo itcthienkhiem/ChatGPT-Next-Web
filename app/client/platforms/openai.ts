f21dcebb729949909a4ae200e5116c81b41e723b
khiem.nthien <itc.thienkhiem@gmail.com>
2023-08-28 10:38:36 +0700

    change endpoint

--- app/client/platforms/openai.ts
+++ app/client/platforms/openai.ts
@@ -28,8 +28,8 @@ export class ChatGPTApi implements LLMApi {
   private disableListModels = true;
 
   path(path: string): string {
-    let openaiUrl = "https://free.churchless.tech";//useAccessStore.getState().openaiUrl;
-    const apiPath = "https://free.churchless.tech";
+    let openaiUrl = "https://openai.1rmb.tk/v1";//useAccessStore.getState().openaiUrl;
+    const apiPath = "https://openai.1rmb.tk/v1";
 
     if (openaiUrl.length === 0) {
       const isApp = !!getClientConfig()?.isApp;
