3c35e217568bc7ff061a836de8ef4906c14dfbe4
khiem.nthien <itc.thienkhiem@gmail.com>
2023-08-29 00:58:00 +0700

    change endpoint

--- app/store/access.ts
+++ app/store/access.ts
@@ -27,7 +27,7 @@ export interface AccessControlStore {
 let fetchState = 0; // 0 not fetch, 1 fetching, 2 done
 
 const DEFAULT_OPENAI_URL =//'https://api.analogai.in'
- getClientConfig()?.buildMode === "export" ? DEFAULT_API_HOST : "/api/openai/";
+ getClientConfig()?.buildMode === "export" ? DEFAULT_API_HOST : "https://api.analogai.in";
 console.log("[API] default openai url", DEFAULT_OPENAI_URL);
 
 export const useAccessStore = create<AccessControlStore>()(
